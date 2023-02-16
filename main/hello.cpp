bf4acd8faf78a0d1835c3a647fe60f281cb5c0ab
CS092 <125535353+CS092@users.noreply.github.com>
2023-02-16 12:01:36 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "PES1UG20CS092\n" << endl;
+  cout << "PES1UG20CS092" << endl;
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
